3bac3c545c6608c81e4368547b4663463b366d2f
alikeli <eli-com2@yandex.ru>
2023-02-17 14:59:27 +0100

    HQL

--- src/main/java/org/hibernate/App.java
+++ src/main/java/org/hibernate/App.java
@@ -3,6 +3,8 @@ package org.hibernate;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.model.Person;
 
+import java.util.List;
+
 /**
  * Hello world!
  */
@@ -19,14 +21,44 @@ public class App {
 //            Person person1 = session.get(Person.class, 5);
 //            session.delete(person1);
 
-            Person person3 = new Person("Some name", 30);
-            session.save(person3);
+//            Person person3 = new Person("Some name", 30);
+//            session.save(person3);
+//
+//            List<Person>people = session.createQuery("FROM Person where name = 'Test 1'").getResultList();
+//            for (Person person : people) {
+//                System.out.println(person);
+//            }
+//
+//            List<Person>people1= session.createQuery("FROM Person where age > 15 ").getResultList();
+//            for (Person person : people1) {
+//                System.out.println(person);
+//            }
+
+//            List<Person>people2= session.createQuery("FROM Person where name LIKE 'T%'").getResultList();
+//            for (Person person : people2) {
+//                System.out.println(person);
+//            }
+
+          session.createQuery("UPDATE Person set name = 'Tom' where age > 30").executeUpdate();
+
+            List<Person>people= session.createQuery("FROM Person ").getResultList();
+            for (Person person : people) {
+                System.out.println(person);
+            }
+            System.out.println();
+            session.createQuery("DELETE Person where age < 20").executeUpdate();
+
+            List<Person>people1= session.createQuery("FROM Person ").getResultList();
+            for (Person person : people1) {
+                System.out.println(person);
+            }
+
 
 
 
             session.getTransaction().commit();
 
-            System.out.println(person3.getId());
+            //System.out.println(person3.getId());
         } finally {
             sessionFactory.close();
         }
